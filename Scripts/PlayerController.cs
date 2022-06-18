7d53ffc8ad498ffa66342d8d1289b27885efb751
Cesar <cesar_a_mar_@hotmail.com>
2022-06-17 20:07:41 -0500

    Salto del Player

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 6f;
-    Rigidbody2D rigibody;
+    Rigidbody2D rigidBody;
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
     }
     void Jump()
     {
-
+        rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
